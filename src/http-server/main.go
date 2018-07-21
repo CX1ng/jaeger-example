638e2ed9680938f809ed7fbfaaf2943505253f55
CX1ng <chuanhang1993@163.com>
2018-07-21 22:59:00 +0800

    http middleware tracing

--- src/http-server/main.go
+++ src/http-server/main.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"context"
-	. "github.com/CX1ng/jaeger-example/src/common"
+	"github.com/CX1ng/jaeger-example/src/common"
 	"github.com/CX1ng/jaeger-example/src/jaeger_test"
 	"github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
@@ -46,61 +46,42 @@ func initJaegerCfg() (io.Closer, error) {
 	return cfg.InitGlobalTracer("Jaeger Http Server")
 }
 
+func tracingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		closer, err := initJaegerCfg()
+		if err != nil {
+
+		}
+		defer closer.Close()
+		spanContext, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
+		var span opentracing.Span
+		if err == nil {
+			span = opentracing.GlobalTracer().StartSpan(r.URL.String(), opentracing.ChildOf(spanContext))
+		} else {
+			span = opentracing.GlobalTracer().StartSpan(r.URL.String())
+		}
+		defer span.Finish()
+		span.SetTag("url", r.URL.String())
+		span.SetTag("method", r.Method)
+		span.LogKV("timestamp", time.Now().Format("2006/01/02 15:04:05"))
+		md := metadata.New(nil)
+		mdWriter := common.MdWriterReader{md}
+		opentracing.GlobalTracer().Inject(span.Context(), opentracing.TextMap, mdWriter)
+		ctx := metadata.NewOutgoingContext(r.Context(), md)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
 func main() {
 	closer, err := initJaegerCfg()
 	if err != nil {
 		panic(err)
 	}
 	defer closer.Close()
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		spanContext, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
-		var span opentracing.Span
-		if err == nil {
-			span = opentracing.StartSpan("http-server-ping", opentracing.ChildOf(spanContext))
-		} else {
-			span = opentracing.GlobalTracer().StartSpan("http-server-ping")
-		}
-		defer span.Finish()
-		span.SetTag("method", r.Method)
-		span.SetTag("url", r.URL)
-		span.SetTag("timestamp", time.Now().Format("2006/01/02 15:04:05"))
-		w.Write([]byte("pong"))
-	})
-	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		var span opentracing.Span
-		spanContext, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		if spanContext != nil {
-			span = opentracing.StartSpan("http-server-test", opentracing.ChildOf(spanContext))
-		} else {
-			span = opentracing.StartSpan("http-server-test")
-		}
-		defer span.Finish()
-		span.SetTag("url", r.URL)
-		w.Write([]byte("test"))
-	})
-	http.HandleFunc("/grpc", func(w http.ResponseWriter, r *http.Request) {
-		// init span
-		var span opentracing.Span
-		spanContext, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		if spanContext != nil {
-			span = opentracing.StartSpan("http-server-test", opentracing.ChildOf(spanContext))
-		} else {
-			span = opentracing.StartSpan("http-server-test")
-		}
-		defer span.Finish()
-		span.SetTag("method", "Get")
-		span.SetTag("url", "/grpc")
-		span.LogKV("event", "Get", "timestamp", time.Now().Unix())
-		ctx := opentracing.ContextWithSpan(context.Background(), span)
-
+	http.Handle("/ping", tracingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("/pong"))
+	})))
+	http.Handle("/grpc", tracingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// get msg
 		if err := r.ParseForm(); err != nil {
 			w.Write([]byte(err.Error()))
@@ -111,26 +92,18 @@ func main() {
 			msg = r.Form["msg"][0]
 		}
 
-		resp, err := grpcConn(ctx, msg)
+		resp, err := grpcConn(r.Context(), msg)
 		if err != nil {
 			w.Write([]byte(err.Error()))
 			return
 		}
 		w.Write([]byte(resp))
-	})
+	})))
 	fmt.Printf("listen on %s\n", listenAddr)
 	http.ListenAndServe(listenAddr, nil)
 }
 
 func grpcConn(ctx context.Context, msg string) (string, error) {
-	span := opentracing.SpanFromContext(ctx)
-	md := metadata.New(nil)
-	mdWriter := MdWriterReader{md}
-	if err := opentracing.GlobalTracer().Inject(span.Context(), opentracing.TextMap, mdWriter); err != nil {
-		return "", err
-	}
-	ctx = metadata.NewOutgoingContext(ctx, md)
-
 	conn, err := grpc.Dial(grpcAddr, grpc.WithInsecure())
 	if err != nil {
 		return "", err
